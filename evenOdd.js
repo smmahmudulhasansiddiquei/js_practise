cbdff36c8bb4957021f4bedbf1f2cc04f0df10e3
smmahmudulhasansiddiquei <mahmudcse03@gmail.com>
2026-07-29 20:44:45 +0600

    even & odd number check

---
+++ evenOdd.js
@@ -0,0 +1,16 @@
+let num = 13;
+
+if(num % 2 === 0){
+    console.log("This number " + num + " is Even");
+}else{
+    console.log("This number " + num + " is Odd");
+}
+
+
+for(let i = 1; i <= num; i++){
+    if(i % 2=== 0){
+        console.log("This number " + i + " is Even");
+    }else{
+        console.log("This number " + i + " is Odd");
+    }
+}
